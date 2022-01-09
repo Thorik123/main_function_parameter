c81d1e546c157b460cd36cb291aa89877575216d
Thorik123 <tronikthorik07@gmail.com>
2022-01-09 17:48:35 +0700

    first commit

---
+++ test/main_function_parameter_test.dart
@@ -0,0 +1,8 @@
+import 'package:main_function_parameter/main_function_parameter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
