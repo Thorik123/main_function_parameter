671a5f7e2a6cf4be06d4597c5ed2cd16fad7efa6
Thorik123 <tronikthorik07@gmail.com>
2022-01-09 19:15:09 +0700

    merubah

--- test/main_function_parameter_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:main_function_parameter/main_function_parameter.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
